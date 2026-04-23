da3fa7363f2f33b05240dbfaf8670f0877a858ea
memen1-arch <nile2.meringolo@live.uwe.ac.uk>
2026-04-24 00:30:06 +0100

    Created a struct type in a header file (.h) that holds 8 rows for the CSV Allocated an array of WaveformSample using malloc() sized for 8 rows Created the IO file and coded it to open and read the CSV, parsing the header row and reading each data row into itsa struct array

--- waveform.h
+++ waveform.h
@@ -1,6 +1,14 @@
+//
+// Created by nilem on 23/04/2026.
+//
 
-#ifndef POWERQUALITYWAVEFORMANALYSER_WAVEFORM_H
-#define POWERQUALITYWAVEFORMANALYSER_WAVEFORM_H
+#ifndef POWER_QUALITY_ANALYSER_COURSEWORK_WAVEFORM_H
+#define POWER_QUALITY_ANALYSER_COURSEWORK_WAVEFORM_H
 
-#endif //POWERQUALITYWAVEFORMANALYSER_WAVEFORM_H
+typedef struct {
+    double timestamp,phase_A_voltage,phase_B_voltage,phase_C_voltage,line_current,frequency,power_factor,thd_percent;
+}WaveformSample; //Struct that holds all 8 fields for a single row of the CSV
 
+WaveformSample* WaveformArray(void);
+int PQLR(void);
+#endif //POWER_QUALITY_ANALYSER_COURSEWORK_WAVEFORM_H
